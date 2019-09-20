cda44c2c7313969d70b9d6a076d00d53a48c7446
Gabriel Santos <gabrielsbs98@gmail.com>
2019-09-20 15:38:00 -0300

    Create function teste

--- luvas/app/src/main/java/com/labbbio/luvas/fragments/HomeFragment.java
+++ luvas/app/src/main/java/com/labbbio/luvas/fragments/HomeFragment.java
@@ -48,6 +48,10 @@ public class HomeFragment extends Fragment {
         return view;
     }
 
+    private void test(){
+        System.out.print("teste");
+    }
+
     private void setClickEvent() {
         messengerCard.setOnClickListener(new View.OnClickListener() {
             @Override
